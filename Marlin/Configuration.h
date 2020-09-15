511201928e959d27803799597462ecdaf86e8422
Alberto Lauretti <a.lauretti@gmail.com>
2020-09-15 22:02:39 +0200

    Calibrato AXIS STEP, LIN_ADVAN

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -742,7 +742,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.97, 78.97, 400, 93 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.97, 78.97, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.566750629, 79.77376590330788, 403.0226700251889, 93 }
 
 /**
  * Default Max Feed Rate (mm/s)
