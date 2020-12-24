f0e7d71b82e38b8a4f0ee396b3a294183c163549
Alberto Lauretti <a.lauretti@gmail.com>
2020-12-24 16:09:28 +0100

    bedsize

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1133,8 +1133,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS -13
 #define Z_MIN_POS 0
-#define X_MAX_POS 254
-#define Y_MAX_POS 240
+#define X_MAX_POS 252
+#define Y_MAX_POS 228
 #define Z_MAX_POS 250
 
 /**
